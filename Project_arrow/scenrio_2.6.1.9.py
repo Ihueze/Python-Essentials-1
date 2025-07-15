29b6288ae2b2b78dcd3d8db80eb8c73e75cb2b0d
Ihueze <davidihueze13@gmail.com>
2025-07-15 21:46:55 +0100

    .\scenrio_2.6.1.9.py

---
+++ Project_arrow/scenrio_2.6.1.9.py
@@ -0,0 +1,15 @@
+# input a float value for variable a here
+a = float(input("Enter the float value for a: "))
+# input a float value for variable b here
+b = float(input("Enter the float value for b: "))
+
+# output the result of addition here
+print("additon", a+b)
+# output the result of subtraction here
+print("subtraction", a-b)
+# output the result of multiplication here
+print("multiplication", a*b)
+# output the result of division here
+print("division", a/b)
+
+print("\nThat's all, folks!")
