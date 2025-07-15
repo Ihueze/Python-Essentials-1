873b05a4f50c38e7dcbdf0a31bd040db2de2cc8a
Ihueze <davidihueze13@gmail.com>
2025-07-15 21:51:09 +0100

    .\scenario_2.6.1.9.py

--- Project_arrow/scenrio_2.6.1.9.py
+++ Project_arrow/scenrio_2.6.1.9.py
@@ -1,3 +1,17 @@
+""" Scenario
+Your task is to complete the code in order to evaluate the results of four basic arithmetic operations.
+
+The results have to be printed to the console.
+
+You may not be able to protect the code from a user who wants to divide by zero. That's okay, don't worry about it for now.
+
+Test your code - does it produce the results you expect?
+
+We won't show you any test data - that would be too simple.
+
+"""
+
+
 # input a float value for variable a here
 a = float(input("Enter the float value for a: "))
 # input a float value for variable b here
