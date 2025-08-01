91480fa6bfc6dc1dbca299e1fc4ec68b904453ba
Ihueze <davidihueze13@gmail.com>
2025-08-01 01:59:08 +0100

    Questions and answers

---
+++ module_3/module_3.1.1.py
@@ -0,0 +1,16 @@
+""" 
+Questions and answers
+
+A programmer writes a program and the program asks questions.
+
+A computer executes the program and provides the answers. The program must be able to react according to the received answers.
+
+Fortunately, computers know only two kinds of answers:
+
+yes, this is true;
+no, this is false.
+You will never get a response like Let me think...., I don't know, or Probably yes, but I don't know for sure.
+
+To ask questions, Python uses a set of very special operators. Let's go through them one after another, illustrating their effects on some simple examples.
+
+"""
