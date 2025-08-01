b915e2d4a6219e794189ee50a77c511b99b30dc8
Ihueze <davidihueze13@gmail.com>
2025-08-01 02:46:12 +0100

    Exercises

---
+++ module_3/module_3.1.3.py
@@ -0,0 +1,29 @@
+"""
+Exercises
+Now let's ask a few questions. Try to guess the answers.
+
+Question #1: What is the result of the following comparison?
+
+2 == 2
+
+Answer:
+True - of course, 2 is equal to 2. 
+Python will answer True (remember this pair of predefined literals, True and False - they're Python keywords, too).
+
+
+Question #2: What is the result of the following comparison?
+
+2 == 2.
+
+Answer:
+This question is not as easy as the first one. 
+Luckily, Python is able to convert the integer value into its real equivalent, and consequently, the answer is True.
+
+Question #3: What is the result of the following comparison?
+
+1 == 2
+
+Answer:
+This should be easy. The answer will be (or rather, always is) False.
+
+"""
