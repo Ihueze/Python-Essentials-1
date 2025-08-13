4aea8ea2f3deb80ed71086ba2ed333ba0d99ce99
Ihueze <davidihueze13@gmail.com>
2025-08-13 22:48:29 +0100

    While loops2

--- module_3/module_3.2.py
+++ module_3/module_3.2.py
@@ -1,29 +1,25 @@
-"""
-Let's go back to the sketch of the algorithm we showed you recently. 
-We're going to show you how to use this newly learned loop to find the largest number from a large set of entered data.
+# A program that reads a sequence of numbers
+# and counts how many numbers are even and how many are odd.
+# The program terminates when zero is entered.
 
+odd_numbers = 0
+even_numbers = 0
 
-Analyze the program carefully. See where the loop starts (line 8). Locate the loop's body and find out how the body is exited:
+# Read the first number.
+number = int(input("Enter a number or type 0 to stop: "))
 
-"""
+# 0 terminates execution.
+while number != 0:
+    # Check if the number is odd.
+    if number % 2 == 1:
+        # Increase the odd_numbers counter.
+        odd_numbers += 1
+    else:
+        # Increase the even_numbers counter.
+        even_numbers += 1
+    # Read the next number.
+    number = int(input("Enter a number or type 0 to stop: "))
 
-# Solution
-
-
-# Store the current largest number here.
-largest_number = -999999999
-
-# Input the first value.
-number = int(input("Enter a number or type -1 to stop: "))
-
-# If the number is not equal to -1, continue.
-while number != -1:
-    # Is number larger than largest_number?
-    if number > largest_number:
-        # Yes, update largest_number.
-        largest_number = number
-    # Input the next number.
-    number = int(input("Enter a number or type -1 to stop: "))
-
-# Print the largest number.
-print("The largest number is:", largest_number)
+# Print results.
+print("Odd numbers count:", odd_numbers)
+print("Even numbers count:", even_numbers)
