a4e782c019e85340f91160e92e202e7b652251a7
Ihueze <davidihueze13@gmail.com>
2025-08-14 00:51:35 +0100

    LAB: Essentials of the while loop - Guess the secret number

--- module_3/module_3.2.1.py
+++ module_3/module_3.2.1.py
@@ -1,84 +1,23 @@
-# The while loop: more examples
+secret_number = 777
 
-"""Let's look at another example employing the while loop. 
-Follow the comments to find out the idea and the solution."""
+print(
+    """
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
 
+# Ask the user for their first guess
+user_number = int(input("Enter a number: "))
 
-# Solution
+# Keep looping until the guess is correct
+while user_number != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    user_number = int(input("Enter a number: "))
 
-# A program that reads a sequence of numbers
-# and counts how many numbers are even and how many are odd.
-# The program terminates when zero is entered.
-
-odd_numbers = 0
-even_numbers = 0
-
-# Read the first number.
-number = int(input("Enter a number or type 0 to stop: "))
-
-# 0 terminates execution.
-while number != 0:
-    # Check if the number is odd.
-    if number % 2 == 1:
-        # Increase the odd_numbers counter.
-        odd_numbers += 1
-    else:
-        # Increase the even_numbers counter.
-        even_numbers += 1
-    # Read the next number.
-    number = int(input("Enter a number or type 0 to stop: "))
-
-# Print results.
-print("Odd numbers count:", odd_numbers)
-print("Even numbers count:", even_numbers)
-
-
-"""
-Certain expressions can be simplified without changing the program's behavior.
-
-Try to recall how Python interprets the truth of a condition, and note that these two forms are equivalent:
-
-while number != 0: and while number:.
-
-The condition that checks if a number is odd can be coded in these equivalent forms, too:
-
-if number % 2 == 1: and if number % 2:.
-
-"""
-
-# Using a counter variable to exit a loop
-# Look at the snippet below:
-
-counter = 5
-while counter != 0:
-    print("Inside the loop.", counter)
-    counter -= 1
-print("Outside the loop.", counter)
-
-"""
-This code is intended to print the string "Inside the loop." and the value stored in the counter variable 
-during a given loop exactly five times. Once the condition has not been met (the counter variable has reached 0),
-the loop is exited, and the message "Outside the loop." as well as the value stored in counter is printed.
-
-But there's one thing that can be written more compactly - the condition of the while loop.
-
-"""
-
-# Can you see the difference?
-
-counter = 5
-while counter:
-    print("Inside the loop.", counter)
-    counter -= 1
-print("Outside the loop.", counter)
-
-
-"""
-Is it more compact than previously? A bit. Is it more legible? That's disputable.
-
-REMEMBER
-
-Don't feel obliged to code your programs in a way that is always the shortest and the most compact. 
-Readability may be a more important factor. Keep your code ready for a new programmer.
-
-"""
+# When guess is correct
+print("Well done, muggle! You are free now.")
