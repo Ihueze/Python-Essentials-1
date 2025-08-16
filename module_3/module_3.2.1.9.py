5717e5b70f249cf8daf607e241fba34088ff788d
Ihueze <davidihueze13@gmail.com>
2025-08-17 00:58:54 +0100

    LAB: The continue statement - the Ugly Vowel Eater

--- module_3/module_3.2.1.9.py
+++ module_3/module_3.2.1.9.py
@@ -1,33 +1,14 @@
-"""
-Objectives
-Familiarize the student with:
+# Prompt the user to enter a word
+# and assign it to the user_word variable.
+user_word = input("Please enter a word: ")
 
-using the break statement in loops;
-reflecting real-life situations in computer code.
-Scenario
-The break statement is used to exit/terminate a loop.
+# convert the user word to upper
+user_word = user_word.upper()
 
-Design a program that uses a while loop and continuously asks the user to enter a word unless the user enters 
-"chupacabra" as the secret exit word, in which case the message "You've successfully left the loop." should be 
-printed to the screen, and the loop should terminate.
+for letter in user_word:
+    # Complete the body of the for loop.
+    if letter in "AEIOU":
+        continue
 
-Don't print any of the words entered by the user. Use the concept of conditional execution and the break statement.
-
-"""
-# Solution
-
-# the secret word
-secret_word = "chupacabra"
-
-# User should enter a word
-user_word = input("Enter a word:")
-
-# looping on till the user enter the right word
-while user_word != secret_word:
-    print("Enter other word")
-    user_word = input("Enter a word:")
-
-# conditional statement
-    if user_word == secret_word:
-        print("You've successfully left the loop.")
-        break
+# print the non-vowel letters
+    print(letter)
